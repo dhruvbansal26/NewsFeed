de0b1ba1b6074f625950bd632117c2774d81bb05
Dhruv Bansal <108138649+dhruvbansal26@users.noreply.github.com>
2023-07-28 13:51:15 +0530

    Update .gitignore

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 3000;
-
 require("dotenv").config();
+const port = process.env.PORT;
 
 const { MongoClient } = require("mongodb");
 const DB_URI = process.env.DB_URI;
